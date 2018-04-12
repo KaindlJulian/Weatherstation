fc5b69979e6565f3477695dc2b29e5e1ced0b737
HIASLSWORKPLACE\Moodeys <gfmerwin@gmail.com>
2018-04-12 18:19:47 +0200

    dailyweather reducer is working with graph

--- weatherstation-webclient/src/components/MainPage.jsx
+++ weatherstation-webclient/src/components/MainPage.jsx
@@ -1,62 +1,85 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { bindActionCreators } from 'redux'
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import {bindActionCreators} from 'redux'
 import {loadInitialData} from '../actions/WeatherActions.js'
-import { connect } from 'react-redux';
-import { push } from "react-router-redux"
+import {connect} from 'react-redux';
+import {push} from "react-router-redux"
 import TopNavbar from './topNavbar.jsx'
-import{Button} from 'mdbreact'
+import {Button} from 'mdbreact'
 import mqtt from 'mqtt'
-import { Bar } from 'react-chartjs-2';
-var client; 
+import {Bar, Line} from 'react-chartjs-2';
+var client;
 
 class mainPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {  
+    this.state = {
       options: {
-        legend: { display: false },
-        title: {
-          display: true,
-          text: 'Predicted world population (millions) in 2050'
+        scales: {
+          xAxes: [
+            {
+              type: 'time',
+              time: {
+                format: "HH:mm",
+                unit: 'hour',
+                unitStepSize: 1,
+                displayFormats: {
+                  'minute': 'HH:mm',
+                  'hour': 'HH:mm',
+                  min: '00:00',
+                  max: '23:59'
+                }
+              }
+            }
+          ]
         }
       }
     };
-  
-    this.nextPath = this.nextPath.bind(this);
+
+    this.nextPath = this
+      .nextPath
+      .bind(this);
   }
-  
-  
+
   componentWillMount() {
     client = mqtt.connect('ws://test.mosquitto.org:8080')
     console.log(client);
-    client.subscribe('presence');
-    this.props.initData(client);
+    client.subscribe('/Station/temperature');
+    this
+      .props
+      .initData(client);
   }
   nextPath(path) {
-    this.props.history.push(path);
+    this
+      .props
+      .history
+      .push(path);
   }
-  
+
   render() {
     return (
       <div>
-        <TopNavbar color={{transparent : 'indigo'}}/>
-        <div style={{paddingTop: '20vh'}}>
-        <Button color="primary" onClick={() => this.nextPath('/Second') }> {this.props.searchString} </Button>
-        <hr/>
-        <Bar data={this.props.data} options={this.state.options}></Bar>
+        <TopNavbar color={{
+          transparent: 'indigo'
+        }}/>
+        <div style={{
+          paddingTop: '20vh'
+        }}>
+          <Button color="primary" onClick={() => this.nextPath('/Second')}>
+            {this.props.searchString}
+          </Button>
+          <hr/>
+          <Line data={this.props.data} options={this.state.options}></Line>
         </div>
-        
-       
+
       </div>
     );
   }
 }
 const mapDispatchToProps = dispatch => bindActionCreators({
-  initData: () => loadInitialData(client),
-
+  initData: () => loadInitialData(client)
 }, dispatch)
 function mapStateToProps(state) {
-  return { cryptoList: state.weather.currencyList, router: state.router, searchString: state.weather.searchString, EURExchange: state.weather.EURExchange, data : state.weather.data }
+  return {precipitation: state.weather.precipitation, temperature: state.weather.temperature, router: state.router, air: state.weather.air, wind: state.weather.wind, data: state.weather.data}
 }
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(mainPage));
